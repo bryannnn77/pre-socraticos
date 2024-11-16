896053566b9bc25462f0268e3cb77f3e32239edf
antunes <163547062+GabrielAntunes14@users.noreply.github.com>
2024-11-16 14:25:14 -0300

    Add files via upload

--- pre-socraticos.js
+++ pre-socraticos.js
@@ -1,1 +1,10 @@
-blob
+let index = 0;
+
+function showNextSlide() {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+    index = (index + 1) % totalSlides;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(showNextSlide, 3000);
